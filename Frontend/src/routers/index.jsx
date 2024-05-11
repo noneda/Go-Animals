1b61f0bbdb3bba9149143f2a0787e351c03b0089
noneda <nonejda@gmail.com>
2024-05-10 21:09:33 -0500

    0.0.1 -> Create App (Oly Structure) -> It have errrors

---
+++ Frontend/src/routers/index.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import paths from "./paths"
+
+const Router = createBrowserRouter(paths)
+
+export default Router;
---
+++ Frontend/src/routers/paths/index.jsx
@@ -0,0 +1,31 @@
+// * Pages
+
+//* Layouts
+
+// * Components
+
+const paths = [
+/*{
+path: "/",
+element : <Login/>,
+errorElement : <Error/>,
+},
+{
+path : "/home",
+element : <Layout/>,
+errorElement : <Error/>,
+children : [
+{
+index : true,
+element : <Home/>
+},
+{
+path: "perfil",
+element : <Perfil/>
+}
+]
+
+}*/
+];
+
+export default paths;
